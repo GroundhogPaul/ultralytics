08ffbe59ddef17aca94a70b83c8b9c1108de5fac
GroundhogPaul <2265546016@qq.com>
2025-06-04 16:29:24 +0800

    training parameters

--- 01_BasicRunPose/02_TrainLapa.py
+++ 01_BasicRunPose/02_TrainLapa.py
@@ -3,24 +3,34 @@ from ultralytics.models import YOLO
 
 
 if __name__ == '__main__':
-    print("I am here.\n")
     # model = YOLO(model='./ultralytics/weights/yolo11n-pose.pt')
     # model.train(data="./ultralytics/cfg/datasets/coco-pose.yaml", epochs=3, imgsz=320)
 
     model = YOLO('./ultralytics/cfg/models/11/yolo11-pose.yaml')
     model.train(
-            # data='ultralytics/cfg/datasets/coco8-pose.yaml',
-            data='./ultralytics/cfg/datasets/lapa8-pose.yaml',
-            # data="./ultralytics/cfg/datasets/lapa-pose.yaml",
-                # cache=False,
-                imgsz=320,
-                epochs=5,
-                # batch=8,
-                # task='pose',
-                close_mosaic=1,
-                # mosaic = 0.0,
+                # data='ultralytics/cfg/datasets/coco8-pose.yaml',
+                data='./ultralytics/cfg/datasets/lapa8-pose.yaml',
+                # data="./ultralytics/cfg/datasets/lapa-pose.yaml",
+                task='pose',
                 pretrained = False,
-                # device='0',
+
+                # cache=False,
+                imgsz=128,
+                translate=0.5,
+                scale=0.5,
+                degrees=45.0,
+                flipud=0.5,
+                # fliplr=0.5,
+                mosaic = 0.0,
+                close_mosaic=-1,
+
+                single_cls=True,
+
+                epochs=5,
+                batch=16,
+                # device = '0',
+                workers = 1,
+
                 # optimizer='SGD', # using SGD
                 # project='runs/train-pose',
                 # name='exp',
