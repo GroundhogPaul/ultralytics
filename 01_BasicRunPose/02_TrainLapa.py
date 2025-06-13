02d3e7e46492eb619a3d5e014ce3673d7185a9cd
GroundhogPaul <2265546016@qq.com>
2025-06-13 11:26:17 +0800

    trainning setting .yaml

--- 01_BasicRunPose/02_TrainLapa.py
+++ 01_BasicRunPose/02_TrainLapa.py
@@ -6,32 +6,12 @@ if __name__ == '__main__':
     # model = YOLO(model='./ultralytics/weights/yolo11n-pose.pt')
     # model.train(data="./ultralytics/cfg/datasets/coco-pose.yaml", epochs=3, imgsz=320)
 
-    model = YOLO('./ultralytics/cfg/models/11/yolo11-pose.yaml')
-    model.train(
-                # data='ultralytics/cfg/datasets/coco8-pose.yaml',
-                data='./ultralytics/cfg/datasets/lapa8-pose.yaml',
-                # data="./ultralytics/cfg/datasets/lapa-pose.yaml",
-                task='pose',
-                pretrained = False,
+    base_cfg = './01_BasicRunPose/02_TrainLapaConf.yaml'
+ 
+    override_params = {
+        # 'epochs': 30,
+        # 'batch': 32
+    }
 
-                # cache=False,
-                imgsz=128,
-                translate=0.5,
-                scale=0.5,
-                degrees=45.0,
-                flipud=0.5,
-                # fliplr=0.5,
-                mosaic = 0.0,
-                close_mosaic=-1,
-
-                single_cls=True,
-
-                epochs=5,
-                batch=16,
-                # device = '0',
-                workers = 1,
-
-                # optimizer='SGD', # using SGD
-                # project='runs/train-pose',
-                # name='exp',
-                )
+    model = YOLO('./ultralytics/cfg/models/11/yolo11n-pose.yaml')
+    model.train(cfg=base_cfg, **override_params)
