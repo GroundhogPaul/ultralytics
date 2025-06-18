6b37dcd1b2dca571c3b24c8973cf48726745b54e
GroundhogPaul <2265546016@qq.com>
2025-06-18 17:21:58 +0800

    Update 04_ToOnnxAndTest.py

--- 01_BasicRunPose/04_ToOnnxAndTest.py
+++ 01_BasicRunPose/04_ToOnnxAndTest.py
@@ -1,20 +1,23 @@
 import cv2
 import onnx
 import onnxruntime
+import onnxslim
 import utilBasicRunPose
 from ultralytics import YOLO
+import os
 
-# Load a model
+# ----- Load a model ----- #
 sModelPath = "./runs/LapaTrain/train_20250613_128x128/weights/best.pt"
-model = YOLO(sModelPath)
-
-model.export(format="onnx", device="cpu") # TODO not stable, sometimes crash
-
 sModelPathOnnx = sModelPath.replace("pt", "onnx")
-onnx_model = YOLO(sModelPathOnnx)
-results = onnx_model("BabySmile.jpg", verbose=False, task='detect', device = "cpu", imgsz=128)  # predict on an image
-# TODO the result cannot plot yet
+model = YOLO(sModelPath, task='pose', verbose=False) 
 
-# img = results[0].plot()
-# cv2.imshow("", img)
-# cv2.waitKey(0)
+# ----- Export to ONNX ----- #
+model.export(format="onnx", device="cpu")
+
+# ----- Load ONNX model and run inference ----- #
+onnx_model = YOLO(sModelPathOnnx, task='pose', verbose=False)
+results = onnx_model("BabySmile.jpg", verbose=False, task='detect', device = "cpu", imgsz=128)  # predict on an image
+
+img = results[0].plot()
+cv2.imshow("", img)
+cv2.waitKey(0)
