f7a541ab1a1374292fcdba1a83a07b1af9ce4067
GroundhogPaul <2265546016@qq.com>
2025-06-19 10:32:15 +0800

    some comment

--- 01_BasicRunPose/03_PredictLapaGetFlops.py
+++ 01_BasicRunPose/03_PredictLapaGetFlops.py
@@ -3,15 +3,18 @@ import utilBasicRunPose
 from ultralytics import YOLO
 from ultralytics.utils.torch_utils import get_flops_with_torch_profiler
 
-# Load a model
+# ----- load a model -----
 sModelPath = "./runs/LapaTrain/train_20250613_128x128/weights/best.pt"
 model = YOLO(sModelPath)
 
-# Predict with the model
+# ----- Predict with the model -----
 results = model("BabySmile.jpg", verbose=False)  # predict on an image
+
+# ----- Plot using Official Class Results(SimpleClass) -----
 img = results[0].plot()
 cv2.imshow("", img)
 cv2.waitKey(0)
 
+# ----- Get GFlops -----
 GFlops = get_flops_with_torch_profiler(model, imgsz=[128, 128])
 print(GFlops)
