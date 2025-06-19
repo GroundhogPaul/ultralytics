16a644536741db98045ee7e6efd60cc5a194c102
GroundhogPaul <2265546016@qq.com>
2025-06-19 10:32:04 +0800

    test export ncnn with pylib ncnn

---
+++ 01_BasicRunPose/05a_TestNcnnMyCode.py
@@ -0,0 +1,55 @@
+import numpy as np
+import utilBasicRunPose
+import ncnn
+import torch
+import cv2
+from ultralytics.utils.ops import non_max_suppression
+from ultralytics.utils.plotting import Annotator
+import os
+
+# ----- Load an image and prepare it for inference ----- #
+sImgIn = "BabySmile.jpg"
+inBGR_HWCh = cv2.imread(sImgIn)
+inRGB_BChHW= utilBasicRunPose.read_image_BChHW(sImgIn, bNormalize=True, bSwapRB=True, bChHW=True)
+inRGB_BChHW = torch.tensor(inRGB_BChHW)
+
+# ----- Load the NCNN model and run inference ----- #
+sFolderModel = "runs/LapaTrain/train_20250613_128x128/weights/best_ncnn_model"
+out = []
+with ncnn.Net() as net:
+    net.load_param(os.path.join(sFolderModel, "model.ncnn.param"))
+    net.load_model(os.path.join(sFolderModel, "model.ncnn.bin"))
+
+    with net.create_extractor() as ex:
+        in0 = inRGB_BChHW.squeeze(0).numpy()
+        ex.input("in0", ncnn.Mat(in0).clone())
+
+        _, out0 = ex.extract("out0")
+        out.append(torch.from_numpy(np.array(out0)).unsqueeze(0))
+
+# ----- Post-process the output ----- #
+outNMS = non_max_suppression(
+            out,
+            0.25,
+            0.7,
+            agnostic=False,
+            max_det=300,
+            classes=None,
+            nc=1,
+        )
+
+print(outNMS[0].shape)
+outNMS = outNMS[0]
+
+# ----- Annotate the image with the results ----- #
+myAnnotator = Annotator(inBGR_HWCh, line_width=2)
+for i in range(outNMS.shape[0]):
+    myAnnotator.box_label(
+        box=outNMS[i, :4].cpu().numpy(),
+        label=f"{int(outNMS[i, 5])} {outNMS[i, 4]:.2f}",
+    )
+    kpts = outNMS[i, 6:6+106*3].cpu().numpy().reshape(-1, 3)
+    myAnnotator.kpts(kpts=kpts)
+
+cv2.imshow("out", myAnnotator.result())
+cv2.waitKey(0)
--- 01_BasicRunPose/utilBasicRunPose.py
+++ 01_BasicRunPose/utilBasicRunPose.py
@@ -1,8 +1,28 @@
 import cv2
 import sys
 from pathlib import Path
+import os
+import numpy as np
 
 # ----- add the parent folder to environment ----- 
 ROOT = Path(__file__).resolve().parents[1]
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
+
+# ----- read in an image and return normalized RGB with shape (1, CH, H, W) -----
+def read_image_BChHW(sImagePath, bNormalize = False, bSwapRB = False, bChHW = False): 
+    # TODO: padding, not ready yet
+    assert os.path.exists(sImagePath), f"Image path does not exist: {sImagePath}"
+   
+    img = cv2.imread(sImagePath)
+    if img is None:
+        raise FileNotFoundError(f"Image not found: {sImagePath}")
+    if bSwapRB:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+    if bNormalize:
+        img = img.astype('float32') / 255.0
+    if bChHW:
+        img = img.transpose((2, 0, 1))  # Change to CxHxW
+        img = img[None, ...]  # Add batch dimension
+        img = np.ascontiguousarray(img)
+    return img
