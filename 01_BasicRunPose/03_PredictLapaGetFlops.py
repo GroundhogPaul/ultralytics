d27f23569d1be3dbdca4f6756596b3db00b48d62
GroundhogPaul <2265546016@qq.com>
2025-07-21 13:51:51 +0800

    add imgsize and task="pose"

--- 01_BasicRunPose/03_PredictLapaGetFlops.py
+++ 01_BasicRunPose/03_PredictLapaGetFlops.py
@@ -5,15 +5,19 @@ from ultralytics.utils.torch_utils import get_flops_with_torch_profiler
 
 # ----- load a model -----
 sModelPath = "./runs/LapaTrain/train_20250613_128x128/weights/best.pt"
-model = YOLO(sModelPath)
+model = YOLO(sModelPath, task="pose")
+imgSize = 176
 
 # ----- Predict with the model -----
-results = model("BabySmile.jpg", verbose=False)  # predict on an image
+# results = model("BabySmile.jpg", imgsz=imgSize, verbose=False)  # predict on an image
+results = model('D:/PxyAI/DataSet/Lapa/valPxy/*jpg', imgsz=imgSize, verbose=False)  # predict on an image
 
 # ----- Plot using Official Class Results(SimpleClass) -----
-img = results[0].plot(line_width=1, kpt_radius=1)
-cv2.imshow("", img)
-cv2.waitKey(0)
+for res in results:
+    img = res.plot(line_width=1, kpt_radius=1)
+    cv2.imwrite(res.path.replace('.jpg', '_plot.jpg'), img)
+    # cv2.imshow("", img)
+    # cv2.waitKey(0)
 
 # ----- Get GFlops ----
 GFlops = get_flops_with_torch_profiler(model, imgsz=[128, 96])
