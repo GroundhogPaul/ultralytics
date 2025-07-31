30f109379f81bdaafc49540ca306c5f025214d97
GroundhogPaul <2265546016@qq.com>
2025-07-31 15:08:31 +0800

    collect my changes

--- 01_BasicRunPose/02_TrainLapa.py
+++ 01_BasicRunPose/02_TrainLapa.py
@@ -3,9 +3,6 @@ from ultralytics.models import YOLO
 
 
 if __name__ == '__main__':
-    # model = YOLO(model='./ultralytics/weights/yolo11n-pose.pt')
-    # model.train(data="./ultralytics/cfg/datasets/coco-pose.yaml", epochs=3, imgsz=320)
-
     base_cfg = './01_BasicRunPose/02_TrainLapaConf.yaml'
  
     override_params = {
@@ -16,7 +13,7 @@ if __name__ == '__main__':
     }
 
     # model = YOLO('./ultralytics/cfg/models/11/yolo11n-pose.yaml')
-    model = YOLO('./ultralytics/cfg/models/11/yolo11n-pose_16ds.yaml') # delete all the 32x
+    model = YOLO('./01_BasicRunPoseSetting/Model-yolo11-pose_16ds.yaml') # delete all the 32x
     # model = YOLO('./runs/LapaTrain/176x1000epoch_MileStone/weights/best.pt') # the 176x miletone 
     # model = YOLO('./runs/LapaTrain/176xToBeResumed/weights/best.pt')
     model.train(cfg=base_cfg, **override_params)
