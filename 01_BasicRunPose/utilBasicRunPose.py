cd1ffc903f13272221bc9aaa7d3283d081562972
GroundhogPaul <2265546016@qq.com>
2025-09-09 14:49:09 +0800

    allow link when find parent folder (which is project folder)

--- 01_BasicRunPose/utilBasicRunPose.py
+++ 01_BasicRunPose/utilBasicRunPose.py
@@ -6,6 +6,7 @@ import numpy as np
 
 # ----- add the parent folder to environment ----- 
 ROOT = Path(__file__).resolve().parents[1]
+ROOT = Path(__file__).parents[1]
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))
 
