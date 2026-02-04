a599a938bc637cb0bcb09e8c321a174f274cd0b1
GroundhogPaul <2265546016@qq.com>
2026-02-04 14:59:02 +0800

    seperate code into 3 conditions: 1. scratch, 2. resume, 3. finetune

--- 01_BasicRunPose/02_TrainLapa.py
+++ 01_BasicRunPose/02_TrainLapa.py
@@ -3,17 +3,30 @@ from ultralytics.models import YOLO
 
 
 if __name__ == '__main__':
-    base_cfg = './01_BasicRunPose/02_TrainLapaConf.yaml'
  
     override_params = {
         'epochs': 1000,
         'batch': 64,
-        # 'resume': True,
-        'resume': False,
+        'name': '640x_Offical',
+        'patience': 0,
+        # 'amp': False,
+        'seed':1,
+        'save_period':10,
+        'val': True
     }
 
-    # model = YOLO('./ultralytics/cfg/models/11/yolo11n-pose.yaml')
-    model = YOLO('./01_BasicRunPoseSetting/Model-yolo11-pose_16ds.yaml') # delete all the 32x
-    # model = YOLO('./runs/LapaTrain/176x1000epoch_MileStone/weights/best.pt') # the 176x miletone 
-    # model = YOLO('./runs/LapaTrain/176xToBeResumed/weights/best.pt')
-    model.train(cfg=base_cfg, **override_params)
+    data = './01_BasicRunPoseSetting/DataSet-lapa-pose-plus.yaml'
+# ----- scratch ----- #
+    base_cfg = './01_BasicRunPoseSetting/02_TrainLapaConf.yaml'
+    model = YOLO('./ultralytics/cfg/models/11/yolo11n-pose.yaml', task='pose')
+    # model = YOLO('./01_BasicRunPoseSetting/Model-yolo11n-pose_16ds_slimmer.yaml', task='pose') # delete all the 32x
+    # model = YOLO('./01_BasicRunPoseSetting/Model-yolo11n-pose_16ds_attention.yaml') # delete 32x ds but remain attention
+# ----- resume ----- #
+    # base_cfg = './01_BasicRunPoseSetting/02_TrainLapaConf_Resume.yaml'
+    # model = YOLO('./runs/LapaTrain/128x_Relu/weights/best.pt')
+# ----- finetune ----- #
+    # base_cfg = './01_BasicRunPoseSetting/02_TrainLapaConf_Finetune.yaml'
+    # model = YOLO('./runs/LapaTrain/160x1000epoch_MileStone/weights/best.pt')
+
+# ----- ----- #
+    model.train(cfg=base_cfg, data = data, **override_params)
